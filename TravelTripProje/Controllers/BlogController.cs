0a48902ffe0754c64da4bbef5ae35ce30f3d8b2d
Haticezzehra <haticezehraorhan@gmail.com>
2023-10-28 23:35:56 +0300

    Add controller and view

---
+++ TravelTripProje/Controllers/BlogController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class BlogController : Controller
+    {
+        Context c = new Context();
+        // GET: Blog
+        public ActionResult Index()
+        {
+            var deger = c.Blogs.ToList();
+            return View(deger);
+        }
+        public ActionResult BlogDetay(int id)
+        {
+         
+            return View();
+        }
+    }
+}
