a10d0cc5a0f45b18a764cf1f8240bd73abeb7180
Haticezzehra <haticezehraorhan@gmail.com>
2023-10-29 22:56:56 +0300

    add partial

--- TravelTripProje/Controllers/HomeController.cs
+++ TravelTripProje/Controllers/HomeController.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
 
 namespace TravelTripProje.Controllers
 {
+   
     public class HomeController : Controller
     {
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
         }
 
       
-
+        public PartialViewResult Partial1()
+        {
+            return PartialView();
+        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
